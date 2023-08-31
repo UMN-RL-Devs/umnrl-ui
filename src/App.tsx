0aa6413b1415b9929763099eaee9e89eaefd65bd
Corbin Donner <Corbyjoe87@gmail.com>
2023-08-31 17:50:59 -0500

    URW-7: Navbar Component (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import { About } from "./components/pages/About";
+import { Home } from "./components/pages/Home";
+import { NoPage } from "./components/pages/NoPage";
+
 
 function App() {
-  return <div className="App">To be implemented</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path ="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
+
---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,29 @@
+import {Outlet, Link } from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <header>
+            <nav className="nav">
+                <Link to="/">UMNRL</Link>
+                <ul>
+                    <li>
+                        <Link to="/About">About</Link>
+                    </li>
+                    <li>
+                        <a href="https://discord.gg/bAe9fQFX" rel="noopener">Discord</a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/esportsumn" rel="noopener">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitch.tv/gopheresports" rel="noopener">Twitch</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet />
+        </header>
+    );
+}
+
+
